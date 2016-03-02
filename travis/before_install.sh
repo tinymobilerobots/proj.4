cc5b6c312053faafa641586cd272564a56fde3d2
Howard Butler <howard@hobu.co>
2016-03-02 12:30:40 -0600

    turn off build matrix for now until we are happy with build status

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_38e0a668034a_key -iv $encrypted_38e0a668034a_iv -in travis/projdocs-private.key.enc -out travis/projdocs-private.key -d
-cp travis/projdocs-private.key ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-echo -e "Host *\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
+pip install --user cpp-coveralls
+./docker.sh
 
 
